910405f828678da93ea1eaadd88ba9b28ec47983
Kevin Wamae <wamaekevin@gmail.com>
2024-05-08 14:49:44 +0300

    refactor: add code description

--- resources-src/functions/aggregate-illumina-extraction-qc.R
+++ resources-src/functions/aggregate-illumina-extraction-qc.R
@@ -1,7 +1,36 @@
-## __b. import fastq extraction reports (by target) ----
+# DESCRIPTION OF R SCRIPT
+# =============================================================================#
+# This R script is specifically tailored to import and process FASTQ extraction reports
+# for SeekDeep data within a defined study, run, and date. The script undertakes several
+# crucial operations to ensure the data is prepared for downstream analysis:
+#
+# 1. Import and Process General Extraction Reports:
+#    Reads a tabulated file containing overall extraction statistics for FASTQ files.
+#    This includes converting all numeric data from string format to numeric to ensure
+#    accurate computations and data handling. The operation focuses on fields from
+#    "totalReadsProcessed" to the last column of the dataset, ensuring all are treated as
+#    numeric values.
+#
+# 2. Import and Enhance Target-Specific Extraction Reports:
+#    Processes another set of extraction reports that detail the extraction statistics
+#    by FASTQ files and specific genetic targets. The script:
+#      - Calculates the total counts per sample to use as a denominator in proportion calculations.
+#      - Computes the frequency of each target as a proportion of the total counts, formatted
+#        as a percentage alongside the absolute count.
+#      - Reshapes the data from long to wide format, where each target becomes a separate column
+#        with combined frequency and count information for easier comparison and analysis.
+#
+# This script utilizes the 'readr', 'dplyr', and 'tidyr' packages from the tidyverse to handle
+# data importation, transformation, and reshaping tasks efficiently. It is essential for researchers
+# or analysts looking to preprocess extraction data for quality checks or preliminary analysis before
+# deeper genetic evaluations.
+#
+# Dependencies: readr, dplyr, tidyr
 # =============================================================================#
 
-### ____i. import report ----
+
+
+# i. extraction reports by FASTQ
 # -----------------------------------------------------------------------------#
 
 raw_extProfile <- read_tsv(paste0(PATH_STUDY, PATH_RUN, PATH_DATE, "reports/allExtractionProfile.tab.txt"),
@@ -12,7 +41,7 @@ raw_extProfile <- read_tsv(paste0(PATH_STUDY, PATH_RUN, PATH_DATE, "reports/allE
 
 
 
-### ____ii. qc summary - by target ----
+# ii. extraction reports by FASTQ and gene
 # -----------------------------------------------------------------------------#
 
 raw_extProfileTarget <- raw_extProfile %>%
